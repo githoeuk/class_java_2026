7e27480d92dbc46182962a8362b1d2362021b1ef
githoeuk <dnrdlwlsdl@gmail.com>
2026-03-10 15:39:39 +0900

    n번째 동기화

--- src/innerClass/Main2.java
+++ src/innerClass/Main2.java
@@ -1,22 +1,22 @@
 package innerClass;
 
-public class Main2 {
-
-    public static void main(String[] args) {
-
-    OuterClass.InnerClass inner = new OuterClass.InnerClass();
-    inner.display();
-
-        // 경우1
-        // inner class가 일반 멤버 클래소 설계된 겨우
-        // 인스턴스 내부 클래스(기본)
-        // - 외부 클래스와 강하게 연결된 상태
-        // - 외부 객체가 있어야 내부 객체를 호출할 수 있음
-
-        // 경우2
-        // Static Inner Class 정적 클래스로 설계된 경우
-        // - 외부 클래스와 이름만 묶인 상태
-        // 외부 객체 생성 없이도 생성이 가능하다
-
-    } // end of main
-}
+//public class Main2 {
+//
+//    public static void main(String[] args) {
+//
+//    OuterClass.InnerClass inner = new OuterClass.InnerClass();
+//    inner.display();
+//
+//        // 경우1
+//        // inner class가 일반 멤버 클래소 설계된 겨우
+//        // 인스턴스 내부 클래스(기본)
+//        // - 외부 클래스와 강하게 연결된 상태
+//        // - 외부 객체가 있어야 내부 객체를 호출할 수 있음
+//
+//        // 경우2
+//        // Static Inner Class 정적 클래스로 설계된 경우
+//        // - 외부 클래스와 이름만 묶인 상태
+//        // 외부 객체 생성 없이도 생성이 가능하다
+//
+//    } // end of main
+//}
